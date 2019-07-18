4aa13497dd443b2459d492fa63e4080a61d92144
Abbey Dickson <dicksonabbey4515@gmail.com>
2019-07-18 15:03:28 +0100

    Login Page

---
+++ MyWebApp/pappy.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return 'Hello world'
+
+@app.route('/whereami')
+def whereami():
+    return 'Ghana!'
+
+@app.route('/FreeStyle')
+def addMoreFunctions():
+    return 'Let the coding Begin!'
+
+@app.route('/addcss')
+def addcss():
+    return render_template('myIndex.html')
+ 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
